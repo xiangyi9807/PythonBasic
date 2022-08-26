c1be4fd66435dd49b65947610e96204999edd87d
xiang <xiangyi9807@163.com>
2022-08-26 15:55:39 +0800

    面向对象，封装

---
+++ 6. 面向对象/test13_类的封装UI应用.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# author: vin
+
+from selenium import webdriver
+import time
+
+class UI(object):
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+        self.driver.implicitly_wait(30)
+
+    def findID(self, ID):
+        return self.driver.find_element_by_id(ID)
+
+    def sendKeys(self, ID, keywords):
+        self.findID(ID).send_keys(keywords)
+
+    def __del__(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    obj = UI('https://www.baidu.com')
+    obj.sendKeys('kw', 'python')
+    time.sleep(3)
