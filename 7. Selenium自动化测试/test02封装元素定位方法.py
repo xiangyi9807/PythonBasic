89c2896ade4f6ac940b69a80b25bacc438654ddb
xiang <xiangyi9807@163.com>
2022-09-06 22:42:45 +0800

    selenium 封装方法

--- 7. Selenium自动化测试/test02封装元素定位方法.py
+++ 7. Selenium自动化测试/test02封装元素定位方法.py
@@ -1,2 +1,25 @@
 #! /usr/bin/env python
 # author: vin
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+# driver.get('')
+# driver.find_element_by_id('')
+
+def findID(driver, ID):
+    return driver.find_element_by_id(ID)
+
+def findName(driver, name):
+    return driver.find_element_by_name(name)
+
+def findTagNames(driver, tagname, index):
+    return driver.find_elements_by_tag_name(tagname)[index]
+
+
+driver.get('https://www.baidu.com')
+findID(driver, 'kw').send_keys('python')
+time.sleep(3)
+driver.quit()
