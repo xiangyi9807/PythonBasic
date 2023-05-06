3c6ca6021fcd5f957462e804681cdb8a6d84a24a
xiang <xiangyi9807@163.com>
2023-05-06 22:01:58 +0800

    delete chapter one

--- 10. Requests实践/10.1/test01.py
+++ 10. Requests实践/10.1/test01.py
@@ -3,9 +3,12 @@
 
 import requests
 
+
 r = requests.request(method='get', url='http://www.baidu.com')
 print(r.status_code)
 # print(r.text)
 # print(r.content)
 print(r.url)
 print(r.headers)
+
+s = requests.Session()
